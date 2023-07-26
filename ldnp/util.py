248ce99518065c8db107eb7c4ea0d17559c05f66
TheAssassin <theassassin@assassinate-you.net>
2023-07-26 16:50:14 +0200

    Improve logging of commands run from this tool

--- ldnp/util.py
+++ ldnp/util.py
@@ -1,5 +1,6 @@
 import os
 import shlex
+import shutil
 import subprocess
 from typing import List
 
@@ -10,5 +11,15 @@ logger = get_logger("util")
 
 
 def run_command(command: List[str | os.PathLike], **kwargs):
-    logger.debug(f"running command {shlex.join(map(str, command))}")
+    # a small but valuable improvement for the log message: let's just resolve the path to the command we try to run
+    # beforehand
+    actual_command = shutil.which(command[0])
+
+    # commands which do not exist (or whose path does not exist) will yield an empty string, so we just replace the
+    # command if which() provided us with some output
+    if actual_command:
+        command[0] = actual_command
+
+    logger.info(f"running command {shlex.join(map(str, command))}")
+
     return subprocess.check_call(command, **kwargs)
