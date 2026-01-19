1e7dbcdf5c145fe47197ad01e29e45ecaf8c7474
TheAssassin <theassassin@assassinate-you.net>
2026-01-19 19:48:45 +0100

    Fix formatting after black update

--- ldnp/templating.py
+++ ldnp/templating.py
@@ -1,7 +1,6 @@
 from jinja2 import Environment, FileSystemLoader
 from pathlib import Path
 
-
 templates_dir_path = Path(__file__).parent / "templates"
 
 jinja_env = Environment(loader=FileSystemLoader(templates_dir_path))
--- ldnp/util.py
+++ ldnp/util.py
@@ -6,7 +6,6 @@ from typing import List
 
 from .logging import get_logger
 
-
 logger = get_logger("util")
 
 
